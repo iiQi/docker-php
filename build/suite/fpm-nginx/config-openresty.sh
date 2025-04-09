f74e29ab244f187e56a158649ac548078544467b
Qi <i@yangqi.co>
2025-04-09 18:25:24 +0800

    优化 openresty 配置文件目录

--- build/suite/fpm-nginx/config-openresty.sh
+++ build/suite/fpm-nginx/config-openresty.sh
@@ -9,6 +9,6 @@ mkdir -p /var/run/openresty
 mkdir -p /etc/openresty/conf.d
 
 curl -o /etc/openresty/nginx.conf https://raw.githubusercontent.com/iiQi/docker-openresty/main/rootfs/usr/local/openresty/nginx/conf/nginx.conf
-curl -o /etc/openresty/conf.d/default.conf https://raw.githubusercontent.com/iiQi/docker-openresty/main/rootfs/etc/nginx/conf.d/default.conf
+curl -o /etc/openresty/conf.d/default.conf https://raw.githubusercontent.com/iiQi/docker-openresty/main/rootfs/usr/local/openresty/nginx/conf/conf.d/default.conf
 curl -o /usr/local/bin/openresty-entrypoint https://raw.githubusercontent.com/iiQi/docker-openresty/main/rootfs/usr/local/bin/entrypoint
 chmod +x /usr/local/bin/openresty-entrypoint
